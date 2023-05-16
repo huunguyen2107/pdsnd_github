e2762b8cfa2daba106215bcabd9e76855eefa69e
huunguyen2107 <huunguyen2107@gmail.com>
2023-05-16 22:56:21 +0700

    add display data

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). 
     while True:
         city = input("\nWould you like to see data from chicago, new york city or washington ?\n")
         if city not in ('chicago', 'new york city', 'washington'):
@@ -190,6 +190,18 @@ def user_stats(df):
     print('-' * 40)
 
 
+def display_data(df,a,b):
+    """Display 5 lines of raw data each time the user says "yes" and stop displaying when he says "no" """
+
+    while True:
+        #Get user input on wether he wants to display raw data
+        user_choice = input("\nWould you like to see raw data ? Type 'yes' to display 5 lines of raw data or 'no' to stop the display.\n")
+        user_choice = user_choice.lower()
+        if user_choice not in ("yes","no"):
+            print("\nPlease, you must choose between 'yes' or 'no'")
+            continue
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -207,5 +219,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
